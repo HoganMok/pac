ca3967d9ecd045cc2f68f3bee49c1ff75e2165ae
Hogan Mok <hoganmok@d142-058-062-016.wireless.sfu.ca>
2023-12-19 17:58:06 -0800

    feat: added inputManager that listens basic key input

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,20 +1,28 @@
 package org.example;
 
+import org.example.manager.InputManager;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Map;
 
 public class Game extends JPanel implements Runnable{
     boolean isRunning;
     Thread gameThread;
+    InputManager inputManager;
+    int playerX,playerY = 0;
     Game(){
         this.setPreferredSize(new Dimension(500,500));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        startGameThread();
     }
 
     public void startGameThread (){
         gameThread = new Thread(this);
         gameThread.start();
+        inputManager = new InputManager();
+        this.addKeyListener(inputManager);
+        this.setFocusable(true);
     }
 
     @Override
@@ -23,8 +31,8 @@ public class Game extends JPanel implements Runnable{
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
-
         while (gameThread != null){
+
             currentTime = System.nanoTime();
             delta += (currentTime - lastTime) / drawInterval;
 
@@ -39,5 +47,23 @@ public class Game extends JPanel implements Runnable{
     }
 
     private void update() {
+        for (Map.Entry<InputManager.direction, Boolean> entry : inputManager.getKeyStates().entrySet()) {
+            if (entry.getValue()){
+                switch (entry.getKey()) {
+                    case Up -> playerY-=10;
+                    case Down -> playerY+=10;
+                    case Left -> playerX-=10;
+                    case Right -> playerX+=10;
+                }
+            }
+        }
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, 16,16);
+        g2.dispose();
     }
 }
---
+++ src/main/java/org/example/manager/InputManager.java
@@ -0,0 +1,42 @@
+package org.example.manager;
+
+import java.awt.event.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InputManager implements KeyListener{
+    public enum direction {
+        Left, Right, Up, Down
+    }
+    private final Map<direction, Boolean> keyStates;
+    public InputManager(){
+       keyStates = new HashMap<>();
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        direction Direction = getDirection(e);
+        keyStates.put(Direction, true);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        direction Direction = getDirection(e);
+        keyStates.put(Direction, false);
+    }
+
+    private direction getDirection(KeyEvent e){
+        return switch (e.getKeyCode()) {
+            case KeyEvent.VK_W, KeyEvent.VK_UP -> direction.Up;
+            case KeyEvent.VK_A, KeyEvent.VK_LEFT -> direction.Left;
+            case KeyEvent.VK_S, KeyEvent.VK_DOWN -> direction.Down;
+            case KeyEvent.VK_D, KeyEvent.VK_RIGHT -> direction.Right;
+            default -> null;
+        };
+    }
+
+    public Map<direction, Boolean> getKeyStates() {return keyStates;}
+}
