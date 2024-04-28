81df03455a3d714ccc305d664562e80415532134
Hogan Mok <hoganmok@Hogans-MacBook-Pro.local>
2024-04-28 13:33:57 -0700

    feat: added jsonManager to read json files

---
+++ src/main/java/org/example/manager/jsonManager.java
@@ -0,0 +1,9 @@
+package org.example.manager;
+
+import java.io.FileReader;
+
+public class jsonManager<ResourcesReader> {
+    static {
+        ResourcesReader reader = new ResourcesReader();
+    }
+}
